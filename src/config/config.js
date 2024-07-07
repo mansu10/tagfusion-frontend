b06b42c57094a8d09c0c257bc2423aa1bac7c1ef
16601314215 <A314>
2024-07-08 00:10:14 +0800

    update web style

--- src/config/config.js
+++ src/config/config.js
@@ -10,11 +10,17 @@ const config = {
 
 // const endpoint_rpc = "http://43.135.26.222:26657"
 // const endpoint_rpc = "http://43.135.33.137:26657"
-const endpoint_rpc = "https://mainnet-beta1.turablockchain.com/rpc"
+const endpoint_rpc = "https://mainnet-beta1.turablockchain.com/rpc/"
 const axiosInstance = axios.create({
     baseURL: config.endpoint,
-    timeout: 10000  // 设置超时时间为 10000 毫秒（10 秒）
+    timeout: 10000,  // 设置超时时间为 10000 毫秒（10 秒）
+    headers: {
+        'Content-Type': 'application/json',
+        // 如果需要身份验证，可以添加 'Authorization': 'Bearer YOUR_TOKEN'
+    } // 设置超时时间为 10000 毫秒（10 秒）
+
 });
+
 
 
 
