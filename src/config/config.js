5f5b335eaeaa3db67ffaafa272b940dd253b9899
16601314215 <A314>
2024-07-07 20:38:04 +0800

    update web style

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const config = {
     // endpoint: 'http://43.135.26.222:8000/'
-    endpoint: 'https://tagfusion.org/tagfusion'
+    endpoint: 'https://tagfusion.org/'
     // endpoint: 'http://127.0.0.1:8000/'
 };
 
