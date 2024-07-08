74f7e2d34b728e729e5690c0ebdef1a4d1127bb1
16601314215 <A314>
2024-07-08 13:21:54 +0800

    testnet

--- src/config/config.js
+++ src/config/config.js
@@ -2,15 +2,15 @@
 import axios from "axios";
 
 const config = {
-    // endpoint: 'http://43.135.26.222:8000/'
-    endpoint: 'https://tagfusion.org/'
+    endpoint: 'http://43.135.26.222:8000/'
+    // endpoint: 'https://tagfusion.org/'
     // endpoint: 'http://127.0.0.1:8000/'
 };
 
 
-// const endpoint_rpc = "http://43.135.26.222:26657"
+const endpoint_rpc = "http://43.135.26.222:26657"
 // const endpoint_rpc = "http://43.135.33.137:26657"
-const endpoint_rpc = "https://mainnet-beta1.turablockchain.com/rpc/"
+// const endpoint_rpc = "https://mainnet-beta1.turablockchain.com/rpc/"
 const axiosInstance = axios.create({
     baseURL: config.endpoint,
     timeout: 10000,  // 设置超时时间为 10000 毫秒（10 秒）
