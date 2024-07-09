d7ae3a31f0aaffabfbb7f137cdeb4bac64fc333d
16601314215 <A314>
2024-07-09 15:54:17 +0800

    update web style

--- src/config/config.js
+++ src/config/config.js
@@ -10,7 +10,7 @@ const config = {
 
 // const endpoint_rpc = "http://43.135.26.222:26657"
 // const endpoint_rpc = "http://43.135.33.137:26657"
-const endpoint_rpc = "https://mainnet-beta1.turablockchain.com/rpc/"
+const endpoint_rpc = "https://rpc-beta1.turablockchain.com"
 const axiosInstance = axios.create({
     baseURL: config.endpoint,
     timeout: 10000,  // 设置超时时间为 10000 毫秒（10 秒）
