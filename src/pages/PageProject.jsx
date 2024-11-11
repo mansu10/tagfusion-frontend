78e69c1dbe43f5a7e045f8a5031ae515c0a8fa38
mansu10 <lixinyu0476@163.com>
2024-11-12 06:41:45 +0800

    update

--- src/pages/PageProject.jsx
+++ src/pages/PageProject.jsx
@@ -86,6 +86,7 @@ const ModalChain = ({ children, chain }) => {
         icon: false,
         toastId: "customId",
       });
+      setIsLoading(true)
       const result = await signingClient.sendTokens(
         address,
         toAddress,
@@ -154,6 +155,7 @@ const ModalChain = ({ children, chain }) => {
       const formData = new FormData();
       formData.append("tura_address", address);
       formData.append("json", jsonStr);
+      formData.append("uuid", json["uuid"]);
       const response = await axiosInstance.post(
         "tagfusion/api/create_up_chain_data/",
         formData,
